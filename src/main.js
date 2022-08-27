704ced73ab0a5bda2c49becf2edad6c57fef884e
tangli <18782994956@163.com>
2022-08-27 17:35:07 +0800

    add logic code from element-ui proj

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css'
+// import { read, writeFileXLSX, utils} from "xlsx";
 
 Vue.config.productionTip = false
+// Vue.prototype.read = read; //设置全局
+// Vue.prototype.writeFileXLSX = writeFileXLSX;//设置全局
+// Vue.prototype.utils = utils;
+
+Vue.use(ElementUI)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+})
