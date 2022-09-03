4f79e71f521a3e9b6cb37c3b600394db3e696134
tangli <18782994956@163.com>
2022-09-03 17:53:27 +0800

    save to cloudbase db

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -2,13 +2,34 @@
 // 定义一些公共的属性和方法
 const httpUrl = "http://39.105.17.99:8080/";
 function initDB() {
-  const cloudbase = require("@cloudbase/node-sdk");
+  // const cloudbase = require("@cloudbase/node-sdk");
+  // const app = cloudbase.init({
+  //   env: "my-vue-app-6gy9qpwhaa42ffed",
+  // });
+  // this.database = app.database();
+
+  // console.log("Global initDB complete");
   const app = cloudbase.init({
     env: "my-vue-app-6gy9qpwhaa42ffed",
   });
-  this.database = app.database();
-  
-  console.log("Global initDB complete");
+
+  app
+    .auth({
+      persistence: "session",
+    })
+    .anonymousAuthProvider()
+    .signIn()
+    .then(() => {
+      // 登录成功
+      console.log("成功");
+      this.database = app.database();
+    })
+    .catch((err) => {
+      // 登录失败
+      console.log("失败");
+    });
+  //匿名登录结束
+  console.log("发起匿名登录"); // true
 }
 // 暴露出这些属性和方法
 
@@ -19,6 +40,6 @@ export default {
     return {
       database,
     };
-  }
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from "element-ui"
 import 'element-ui/lib/theme-chalk/index.css'
-import global_ from './components/Global'
+import global_ from './components/Global.vue'
 
 // import { read, writeFileXLSX, utils} from "xlsx";
 
+//匿名登录
+import cloudbase from '@cloudbase/js-sdk';
+const app = cloudbase.init({
+  env: "my-vue-app-6gy9qpwhaa42ffed"
+});
+
+app
+  .auth({
+    persistence: "session"
+  })
+  .anonymousAuthProvider()
+  .signIn()
+  .then(() => {
+    // 登录成功
+    console.log("成功")
+  })
+  .catch((err) => {
+    // 登录失败
+    console.log("失败")
+  });
+//匿名登录结束
+console.log("发起匿名登录"); // true
 Vue.config.productionTip = false
 // Vue.prototype.read = read; //设置全局
 // Vue.prototype.writeFileXLSX = writeFileXLSX;//设置全局
