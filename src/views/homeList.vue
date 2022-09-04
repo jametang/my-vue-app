07d1ada4e67bb01b3e8ea7dda7af4f98b72a4d56
tangli <18782994956@163.com>
2022-09-04 17:42:52 +0800

    支持带参数跳转

--- src/views/homeList.vue
+++ src/views/homeList.vue
@@ -2,10 +2,29 @@
   <div id="homelist">
     <h1>I am title</h1>
     <div>Home List</div>
+    <div>
+      <button @click="jumpto">跳转到detailorder</button>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {}
+export default {
+  data() {
+    return {
+      arg0: "我是从hostlist过来的",
+    };
+  },
+  methods: {
+    jumpto() {
+      this.$router.push({
+        path: "preview",
+        query: {
+          desc: this.arg0,
+        },
+      });
+    },
+  },
+};
 </script>
 
